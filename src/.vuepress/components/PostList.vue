e21fd8f75d2a4d742fb50f1ea716f75a3a5caa94
Satoshi Nanya <nancy@SatoshinoMacBook-puro.local>
2019-01-28 21:16:51 +0900

    first commit

---
+++ src/.vuepress/components/PostList.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="post in posts">
+            <h2><a :href="post.path">{{post.title}}</a></h2>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    computed: {
+        posts() {
+            return this.$site.pages
+                .filter(post => post.regularPath.startsWith('/_posts/'))
+                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
+        }
+    }
+}
+</script>
---
+++ src/_posts/20190127/article-test.md
@@ -0,0 +1,8 @@
+---
+title: 記事テスト
+date: 2019-01-27
+categories:
+  - VuePress
+---
+
+こんにちは。記事です。
