f79ed049c18ec14e81c7c586fe9043bf9c5f511f
Satoshi Nanya <nancy@SatoshinoMacBook-puro.local>
2019-02-03 18:12:29 +0900

    公開日設定

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -1,14 +1,28 @@
 <style lang="scss" scoped>
-.post-list__link {
-  color: #666;
+.post-list {
+  padding-left: 0;
+  &__item {
+    list-style-type: none;
+  }
+  &__title {
+    border-bottom: none;
+  }
+  &__link {
+    color: #666;
+  }
+  &__meta {
+    color: #999;
+    font-size: 0.8rem;
+  }
 }
 </style>
 
 <template>
   <ul class="post-list">
     <li class="post-list__item" v-for="post in posts">
-      <h2>
+      <h2 class="post-list__title">
         <a class="post-list__link" :href="post.path">{{post.title}}</a>
+        <p class="post-list__meta">Published: <span class="post-list__date">{{ post.date }}</span></p>        
       </h2>
     </li>
   </ul>
@@ -17,8 +31,14 @@
 export default {
   computed: {
     posts() {
+      console.info(this.$site.pages);
       return this.$site.pages
         .filter(post => post.regularPath.startsWith("/_posts/"))
+        .map(post => {
+          const date = new Date(post.frontmatter.date);
+          post.date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+          return post;
+        })
         .sort(
           (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
         );
