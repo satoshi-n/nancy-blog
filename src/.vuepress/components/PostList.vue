431d8a855db9ab388457cc050e3f9c0e89a595b4
Satoshi Nanya <nancy@SatoshinoMacBook-puro.local>
2019-02-03 17:49:34 +0900

    Firebase Hostingサイトにスタードメインのドメインを設定する

--- src/.vuepress/components/PostList.vue
+++ src/.vuepress/components/PostList.vue
@@ -1,18 +1,28 @@
+<style lang="scss" scoped>
+.post-list__link {
+  color: #666;
+}
+</style>
+
 <template>
-    <ul>
-        <li v-for="post in posts">
-            <h2><a :href="post.path">{{post.title}}</a></h2>
-        </li>
-    </ul>
+  <ul class="post-list">
+    <li class="post-list__item" v-for="post in posts">
+      <h2>
+        <a class="post-list__link" :href="post.path">{{post.title}}</a>
+      </h2>
+    </li>
+  </ul>
 </template>
 <script>
 export default {
-    computed: {
-        posts() {
-            return this.$site.pages
-                .filter(post => post.regularPath.startsWith('/_posts/'))
-                .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
-        }
+  computed: {
+    posts() {
+      return this.$site.pages
+        .filter(post => post.regularPath.startsWith("/_posts/"))
+        .sort(
+          (a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+        );
     }
-}
+  }
+};
 </script>
--- src/_posts/20190127/article-test.md
+++
@@ -1,8 +0,0 @@
----
-title: 記事テスト
-date: 2019-01-27
-categories:
-  - VuePress
----
-
-こんにちは。記事です。
