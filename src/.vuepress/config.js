7bc4716acdd6a68dd873acee0c0a837ea43b207e
Satoshi Nanya <nancy@SatoshinoMacBook-puro.local>
2019-02-04 19:54:59 +0900

    locale、title、description、gaの設定

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,4 +2,12 @@ module.exports = {
   plugins: [
     '@vuepress/blog',
   ],
+  locales: {
+    '/': {
+      lang: 'ja',
+      title: 'nansystem',
+      description: 'js,vue,cssについての学びをアウトプットするブログです',
+    }
+  },
+  ga: 'UA-49035499-6',
 }
